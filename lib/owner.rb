4dd06217ec87bbe19d16707364c5a9b1fa4be6d1
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-01 22:01:16 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
-  # code goes here
+  def initialize(name)
+    @species = "human"
+    @name = name
+    @@owners << self
+  end
+
+  #Variables
+  @@owners = []
+  
+  def name
+    @name
+  end
+
+  def species
+    @species
+  end
+
+  def species=(species)
+    @species.dup.freeze
+  end
+
+  def count
+    @@owners.count
+  end
+
+  #methods
+  def reset_all
+    @@owners.clear
+  end
+
+  
 end
