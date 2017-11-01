4b26e6f5dcb306b0573dac8baa6aa42737a6ef9f
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-01 22:33:04 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -80,6 +80,18 @@ class Owner
     end
   end
 
+  def sell_pets
+    @pets[:fishes].each do |fish|
+      fish.mood = "nervous"
+    end
+    @pets[:cats].each do |cat|
+      cat.mood = "nervous"
+    end
+    @pets[:dogs].each do |dog|
+      dog.mood = "nervous"
+    end
+  end
+
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
