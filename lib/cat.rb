e2129f1b5de10490b85dab91a5c476fd0c6c730f
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-01 21:53:41 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,10 +2,18 @@ class Cat
 
   def initialize(name)
     @name = name
+    @mood = "nervous"
   end
 
   #Variables
   def name
     @name
   end
+
+  def mood
+    @mood
+  end
+
+  #methods
+  
 end
